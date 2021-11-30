da7c0f75704e5cffbaee0d509f79eec9a8e441de
TheBoltThrower <hrvoje.kuzmanovic@gmail.com>
2021-11-30 13:59:53 +0100

    add dropdown menu, work on JS function

--- JS/project.js
+++ JS/project.js
@@ -1,2 +1,19 @@
-console.log('Project');
+var cards = document.querySelectorAll('.card-item')
+
+var logo = document.querySelector('.header-logo');
+var popup = document.querySelector('.user-menu');
+
+cards.forEach( card => {
+    card.classList.add('hover_class1');
+});
+console.log(cards);
+
+
+logo.addEventListener('click', () => { 
+    if(popup) {
+        popup.classList.add('user-disp');
+        } else {
+            remove
+        }
+    });
 
