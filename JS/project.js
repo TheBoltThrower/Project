e5ec86ec0bcfe5368ee335f2d16a8313f8c329a4
TheBoltThrower <hrvoje.kuzmanovic@gmail.com>
2021-12-01 15:24:22 +0100

    add Eventlisteren to logo menu

--- JS/project.js
+++ JS/project.js
@@ -3,17 +3,17 @@ var cards = document.querySelectorAll('.card-item')
 var logo = document.querySelector('.header-logo');
 var popup = document.querySelector('.user-menu');
 
+
 cards.forEach( card => {
     card.classList.add('hover_class1');
 });
-console.log(cards);
+
 
 
 logo.addEventListener('click', () => { 
-    if(popup) {
-        popup.classList.add('user-disp');
-        } else {
-            remove
-        }
-    });
+    popup.classList.add('user-disp');
+});
 
+popup.addEventListener('click', () => {
+    popup.classList.remove('user-disp');
+});
